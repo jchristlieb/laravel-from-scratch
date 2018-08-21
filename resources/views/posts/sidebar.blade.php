9e612c0473f21e850582d658d5e82865bfebc205
Jan <mail@janchristlieb.de>
2018-08-21 11:39:55 +0200

    added a dynamic post sidebar

---
+++ resources/views/posts/sidebar.blade.php
@@ -0,0 +1,18 @@
+<div class="row mt-4 mb-4">
+    <div class="col-12">
+        <div class="card mb-4">
+            <div class="card-body">
+                <h3>Hello</h3>
+                <p>This is a just a simple blog.</p>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="card-body">
+                <h3>Archive</h3>
+                <ul>
+                    <li>Monat</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
