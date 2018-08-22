e8cd7babab438ac36adc550bcee1a756e506b3c3
Jan <mail@janchristlieb.de>
2018-08-22 14:13:00 +0200

    added post->comment relationship

--- app/Post.php
+++ app/Post.php
@@ -2,22 +2,15 @@
 
 namespace App;
 
+use App\Comment;
+
 class Post extends Model
 {
 
-    // declare relationship: A post has many comments
-    public function comments(){
-
+    // declare relationship to Comments: A post has many comments
+    public function comments()
+    {
         return $this->hasMany(Comment::class);
-    }
-
-    // ???
-    public function addComment($body){
-
-        Comment::create([
-            'body' => $body,
-            'post_id' => $this->id,
-        ]);
     }
 
 
